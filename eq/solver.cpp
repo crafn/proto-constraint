2691df57521579dcc5de30645a65cc30370918e5
tuukka <tuukka.pensala@gmail.com>
2014-08-19 17:31:16 +0300

    Replaced op::OptimizeVar with custom MaximizeVar

--- eq/solver.cpp
+++ eq/solver.cpp
@@ -1,6 +1,78 @@
 #include "solver.hpp"
 
 namespace eq {
+namespace detail {
+
+/// Custom optimizer for or-tools solver
+/// Optimizes for solution which has biggest value for given IntVar
+/// Needs only 32 steps for int32 range, while or-tools MakeMaximize needs 4 billion
+/// @note May miss the absolute maximum. This can be prevented by using 64 steps
+class MaximizeVar : public op::SearchMonitor {
+public:
+	MaximizeVar(op::Solver& solver, op::IntVar* var)
+		: SearchMonitor(&solver), var_(var) { }
+	virtual ~MaximizeVar() { }
+	
+	int64 best() const { return best_; }
+
+	op::IntVar* Var() const { return var_; }
+
+	virtual void EnterSearch()
+	{
+		has_init_solution_= false;
+		best_= kint64min;
+	}
+
+	virtual void RestartSearch() { ApplyBound(); }
+	virtual void RefuteDecision(op::Decision* d) { ApplyBound(); }
+	
+	virtual bool AtSolution()
+	{
+		int64 val= var_->Value();
+		if (!has_init_solution_ || val > best_)
+			best_= val;
+		has_init_solution_= true;
+	}
+
+	virtual bool AcceptSolution()
+	{
+		int64 val= var_->Value();
+		if (!has_init_solution_)
+			return true;
+		else
+			return val > best_;
+	}
+
+	virtual std::string Print() const { return ""; }
+	virtual std::string DebugString() const { return ""; }
+
+	virtual void Accept(op::ModelVisitor* const visitor) const
+	{
+		visitor->BeginVisitExtension(op::ModelVisitor::kObjectiveExtension);
+		visitor->VisitIntegerExpressionArgument(
+				op::ModelVisitor::kExpressionArgument,
+				var_);
+		visitor->EndVisitExtension(op::ModelVisitor::kObjectiveExtension);
+	}
+	 
+	void ApplyBound()
+	{
+		if (has_init_solution_) {
+			var_->SetMin(best_ + 1);
+		}
+	}
+	 
+private:
+	op::IntVar* const var_= nullptr;
+	int64 best_= 0;
+	bool has_init_solution_= false;
+	
+	DISALLOW_COPY_AND_ASSIGN(MaximizeVar);
+};
+
+	
+
+} // detail
 
 void Solver::addVar(int& ref)
 {
@@ -11,7 +83,7 @@ void Solver::apply()
 {
 	/// @todo Should undo this after solving
 	auto success_amount= solver.MakeSum(successAmounts)->Var();
-	auto optimizer= solver.MakeMaximize(success_amount, 100);
+	auto optimizer= solver.RevAlloc(new detail::MaximizeVar(solver, success_amount));
 
 	std::vector<op::IntVar*> vars;
 	for (auto&& v : intVars) {
